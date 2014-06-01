b4a6b981ecbd26e53edfa35a473e3980c817ae78
AllaJohnson <deep_ampik@bk.ru>
2014-06-01 16:12:45 +0400

    Add a StsticPages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fantom::Application.config.secret_key_base = '14158c370d69d0b964013f54f96369b2236786cc9bfa6fc175187447bb13016dca4f4128df142901a3353318c4f682b37f87fcd31c1706369884c983b96a8660'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fantom::Application.config.session_store :cookie_store, key: '_fantom_session'
