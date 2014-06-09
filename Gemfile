a07e112bb204a91c6275b29ff5b14e1ee6946d85
AllaJohnson <deep_ampik@bk.ru>
2014-06-09 18:54:57 +0400

    Create UsersController and routes

--- Gemfile
+++ Gemfile
@@ -3,8 +3,11 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
+# Use Bootstrap
+gem 'bootstrap-sass', '2.3.2.0'
+
 # Use sqlite3 as the database for Active Record
-#gem 'sqlite3'
+gem 'sqlite3', '1.3.8'
 
 # Use Devise
 gem 'devise'
@@ -16,7 +19,6 @@ gem 'activeadmin', github: 'gregbell/active_admin'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
-  # gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Fantom::Application.routes.draw do
+  get "users/new"
   devise_for :admin_users, ActiveAdmin::Devise.config
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contacts"
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contacts', to: 'static_pages#contacts', via: 'get'
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
